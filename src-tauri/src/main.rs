26fc5be2c8fd8f1989c5b027efea95bd846ce099
edelweiss <creamzy1337@gmail.com>
2024-04-10 02:06:39 +0300

    Initial commit

---
+++ src-tauri/src/main.rs
@@ -0,0 +1,54 @@
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+use tauri::{AppHandle, Manager, PhysicalSize, Size};
+
+fn main() {
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![href_auth, href_main, auth])
+        .run(tauri::generate_context!())
+        .expect("error: run()");
+}
+
+#[tauri::command]
+fn href_auth(app: AppHandle) {
+    if app.get_window("main").is_none() {
+        return ();
+    }
+
+    let main = app.get_window("main").unwrap();
+    main.set_title("Авторизация").expect("error: set_title()");
+    main.set_resizable(false).expect("error: set_resizable()");
+    main.set_size(Size::Physical(PhysicalSize {
+        width: 400,
+        height: 300,
+    }))
+    .expect("error: set_size()");
+    main.center().expect("error: center()");
+}
+
+#[tauri::command]
+fn href_main(app: AppHandle) {
+    if app.get_window("main").is_none() {
+        return ();
+    }
+
+    let main = app.get_window("main").unwrap();
+    main.set_title("Информаионная система тира")
+        .expect("error: set_title()");
+    main.set_resizable(true).expect("error: set_resizable()");
+    main.set_size(Size::Physical(PhysicalSize {
+        width: 1280,
+        height: 720,
+    }))
+    .expect("error: set_size()");
+    main.center().expect("error: center()");
+}
+
+#[tauri::command]
+fn auth(login: String, password: String) -> bool {
+    if login == "login" && password == "password" {
+        return true;
+    } else {
+        return false;
+    }
+}
---
+++ src/routes/main/+page.svelte
@@ -0,0 +1,6 @@
+<script type="module" src>
+	import { invoke } from '@tauri-apps/api/tauri';
+	invoke('href_main');
+</script>
+
+<div>main</div>
